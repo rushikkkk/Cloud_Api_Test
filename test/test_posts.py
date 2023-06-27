e2599148d4627b95b62832fc5d14b21c28f213bb
bane <test@mail.ru>
2023-06-27 22:22:37 +0300

    Добавил коментарии к assert

--- test/test_posts.py
+++ test/test_posts.py
@@ -8,31 +8,31 @@ from random import randint
 class TestPosts:
     def test_expected_200_get_all_posts(self, api: ApiClient, get_schema) -> None:
         res = api.get_posts()
-        assert res.status_code == 200
-        assert Schema.validate(res.json(), get_schema) is True
+        assert res.status_code == 200, "status code != 200"
+        assert Schema.validate(res.json(), get_schema) is True, "Not match schema"
 
     def test_expected_200_get_one_posts(self, api: ApiClient, get_one_schema) -> None:
         data = randint(1, 50)
         res = api.get_posts(path_param=f"/{data}")
-        assert res.status_code == 200
-        assert res.json()["id"] == data
-        assert Schema.validate(res.json(), get_one_schema) is True
+        assert res.status_code == 200, "status code != 200"
+        assert res.json()["id"] == data, "Not match id"
+        assert Schema.validate(res.json(), get_one_schema) is True,"Not match schema"
 
     @pytest.mark.xfail
     def test_expect_not_found_posts(self, api: ApiClient):
         data = randint(500, 1000)
         res = api.get_posts(path_param=f"/{data}")
-        assert res.status_code == 404
-        assert res.json() == {}
+        assert res.status_code == 404, "status code != 404"
+        assert res.json() == {}, "json not empty"
 
     def test_expected_201_create_posts(self, api: ApiClient, post_schema) -> None:
         data = {"userId": 6, "title": "test title", "body": "test body"}
         res = api.post_posts(data)
-        assert res.status_code == 201
-        assert Schema.validate(res.json(), post_schema) is True
+        assert res.status_code == 201, "status code != 404"
+        assert Schema.validate(res.json(), post_schema) is True ,"Not match schema"
 
     def test_expected_200_delete_posts(
         self, create_posts: dict, api: ApiClient
     ) -> None:
         res = api.delete_posts(f"/{create_posts['id']}")
-        assert res.status_code == 200
+        assert res.status_code == 200, "status code != 200"
